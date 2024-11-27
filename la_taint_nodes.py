8816d9f1c8923b18461fc0560d83f39235072d80
Simone Tiraboschi <stirabos@redhat.com>
2024-11-27 11:32:45 +0100

    Add sorting logic
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- la_taint_nodes.py
+++ la_taint_nodes.py
@@ -2,13 +2,20 @@
 from kubernetes import client, config
 from prometheus_api_client import PrometheusConnect
 from openshift.dynamic import DynamicClient
+from collections import OrderedDict
 
+import math
 import os
 import requests
 
 
 TAINT_KEY = "la-taint-psi-cpu"
 PSI_CPU_QUERY = 'rate(node_pressure_cpu_waiting_seconds_total[1m])'
+MAX_TAINTS_RATIO = 0.50
+HARD_TAINT = 'NoSchedule'
+SOFT_TAINT = 'PreferNoSchedule'
+HARD_TAINT_THRESHOLD = 50.0
+SOFT_TAINT_THRESHOLD = 20.0
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -21,8 +28,127 @@ class BearerAuth(requests.auth.AuthBase):
         return r
 
 
-def func(x):
-    return x + 1
+def get_max_hard_taints(wnode_num):
+    return int(math.floor(wnode_num*MAX_TAINTS_RATIO))
+
+
+def get_max_soft_taints(wnode_num, htaints_num):
+    return int(math.floor(wnode_num*MAX_TAINTS_RATIO))-htaints_num
+
+
+def get_hard_taint():
+    return {'effect': HARD_TAINT, 'key': TAINT_KEY, 'value': HARD_TAINT}
+
+
+def get_soft_taint():
+    return {'effect': SOFT_TAINT, 'key': TAINT_KEY, 'value': SOFT_TAINT}
+
+
+def get_worker_nodes():
+    worker_nodes = {}
+    v1 = client.CoreV1Api()
+    ret = v1.list_node(label_selector='node-role.kubernetes.io/worker')
+    for i in ret.items:
+        worker_nodes[i.metadata.name] = {
+            "existing_taint": None,
+            "cpu_pressure": 0.0,
+            "proposed_taint": None
+        }
+        if i.spec.taints is not None:
+            for t in i.spec.taints:
+                if t.key == TAINT_KEY:
+                    worker_nodes[i.metadata.name]["existing_taint"] = t
+    return worker_nodes
+
+
+def get_prom_url(dyn_client):
+    v1_routes = dyn_client.resources.get(
+        api_version='route.openshift.io/v1',
+        kind='Route'
+    )
+    prometheus_route = v1_routes.get(
+        name="prometheus-k8s",
+        namespace="openshift-monitoring"
+    )
+
+    return f"https://{prometheus_route.status.ingress[0].host}"
+
+
+def get_metric_for_nodes(prometheus_url, token_content, worker_nodes):
+    prom = PrometheusConnect(
+        url=prometheus_url,
+        disable_ssl=False,
+        auth=BearerAuth(token_content)
+    )
+    cm = prom.custom_query(query=PSI_CPU_QUERY)
+    for m in cm:
+        node = m.get('metric').get('instance')
+        if node in worker_nodes:
+            worker_nodes[node]["cpu_pressure"] = float(m.get('value')[1]) * 100
+    return worker_nodes
+
+
+def sort_nodes_by_metric(worker_nodes):
+    return OrderedDict(
+        sorted(
+            worker_nodes.items(),
+            key=lambda x: x[1]['cpu_pressure'],
+            reverse=True
+        )
+    )
+
+
+def set_expected_taints(worker_nodes_sorted):
+    wnode_num = len(worker_nodes_sorted)
+    max_hard_taints = get_max_hard_taints(wnode_num)
+    max_soft_taints = max_hard_taints
+    hard_taints = 0
+    soft_taints = 0
+    for node_name in worker_nodes_sorted:
+        metric_value = worker_nodes_sorted[node_name]['cpu_pressure']
+        if (metric_value >= HARD_TAINT_THRESHOLD
+                and hard_taints < max_hard_taints):
+            worker_nodes_sorted[node_name]['proposed_taint'] = get_hard_taint()
+            hard_taints = hard_taints + 1
+            max_soft_taints = get_max_soft_taints(wnode_num, hard_taints)
+            if hard_taints >= max_hard_taints:
+                break
+        elif (metric_value >= SOFT_TAINT_THRESHOLD
+              and soft_taints < max_soft_taints):
+            worker_nodes_sorted[node_name]['proposed_taint'] = get_soft_taint()
+            soft_taints = soft_taints + 1
+            if soft_taints >= max_soft_taints:
+                break
+        else:
+            break
+
+
+def apply_taint(node_name, proposed_taint):
+    # TODO: implement me
+    print("apply")
+
+
+def remove_taint(node_name, proposed_taint):
+    # TODO: implement me
+    print("remove")
+
+
+def update_taint(node_name, proposed_taint):
+    # TODO: implement me
+    print("update")
+
+
+def compute_apply_patches(worker_nodes_sorted):
+    for node_name in worker_nodes_sorted:
+        existing_taint = worker_nodes_sorted[node_name]['existing_taint']
+        proposed_taint = worker_nodes_sorted[node_name]['proposed_taint']
+        if existing_taint is None and proposed_taint is not None:
+            apply_taint(node_name, proposed_taint)
+        elif existing_taint is not None and proposed_taint is None:
+            remove_taint(node_name, proposed_taint)
+        elif (existing_taint is not None and proposed_taint is not None
+              and existing_taint['effect'] != proposed_taint['effect']):
+            update_taint(node_name, proposed_taint)
 
 
 def main():
@@ -38,48 +164,21 @@ def main():
         # | jq -r '.data["ca.crt"]' > ca.crt
         ca_crt = 'ca.crt'
 
-    k8s_client = client.ApiClient()
-    dyn_client = DynamicClient(k8s_client)
-
-    worker_nodes = {}
-    v1 = client.CoreV1Api()
-    ret = v1.list_node(label_selector='node-role.kubernetes.io/worker')
-    for i in ret.items:
-        worker_nodes[i.metadata.name] = {
-            "existing_taint": None,
-            "cpu_pressure": 0.0
-        }
-        if i.spec.taints is not None:
-            for t in i.spec.taints:
-                if t.key == TAINT_KEY:
-                    worker_nodes[i.metadata.name]["existing_taint"] = t
-
     os.environ["REQUESTS_CA_BUNDLE"] = ca_crt
-
-    v1_routes = dyn_client.resources.get(
-        api_version='route.openshift.io/v1',
-        kind='Route'
-    )
-    prometheus_route = v1_routes.get(
-        name="prometheus-k8s",
-        namespace="openshift-monitoring"
-    )
-
-    prometheus_url = f"https://{prometheus_route.status.ingress[0].host}"
     with open(token, 'r') as file:
         token_content = file.read()
 
-    prom = PrometheusConnect(
-        url=prometheus_url,
-        disable_ssl=False,
-        auth=BearerAuth(token_content)
-    )
-    cm = prom.custom_query(query=PSI_CPU_QUERY)
-    for m in cm:
-        node = m.get('metric').get('instance')
-        if node in worker_nodes:
-            worker_nodes[node]["cpu_pressure"] = float(m.get('value')[1]) * 100
-    print(f"worker nodes: {worker_nodes}")
+    k8s_client = client.ApiClient()
+    dyn_client = DynamicClient(k8s_client)
+
+    worker_nodes = get_worker_nodes()
+
+    prometheus_url = get_prom_url(dyn_client)
+    get_metric_for_nodes(prometheus_url, token_content, worker_nodes)
+
+    worker_nodes_sorted = sort_nodes_by_metric(worker_nodes)
+    set_expected_taints(worker_nodes_sorted)
+    compute_apply_patches(worker_nodes_sorted)
 
 
 if __name__ == '__main__':
--- test/test_la_taint_nodes.py
+++ test/test_la_taint_nodes.py
@@ -1,5 +1,42 @@
-from la_taint_nodes import *
+import pytest
+from la_taint_nodes import get_max_hard_taints, get_max_soft_taints
 
-def test_answer():
-    assert func(3) == 4
 
+@pytest.mark.parametrize(
+    "wnode_num,expected",
+    [
+        (1, 0),
+        (2, 1),
+        (3, 1),
+        (4, 2),
+        (5, 2),
+        (6, 3)
+    ]
+)
+def test_get_max_hard_taints(wnode_num, expected):
+    assert get_max_hard_taints(wnode_num) == expected
+
+
+@pytest.mark.parametrize(
+    "wnode_num,hard_taints,expected",
+    [
+        (1, 0, 0),
+        (2, 0, 1),
+        (2, 1, 0),
+        (3, 0, 1),
+        (3, 1, 0),
+        (4, 0, 2),
+        (4, 1, 1),
+        (4, 2, 0),
+        (5, 0, 2),
+        (5, 1, 1),
+        (5, 2, 0),
+        (6, 0, 3),
+        (6, 1, 2),
+        (6, 2, 1),
+        (6, 3, 0),
+    ]
+)
+def test_get_max_soft_taints(wnode_num, hard_taints, expected):
+    assert hard_taints <= get_max_hard_taints(wnode_num)
+    assert get_max_soft_taints(wnode_num, hard_taints) == expected
